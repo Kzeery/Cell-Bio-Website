f910bf155df82424cf6bca2426f0df2856ce1bf1
Yousif Kzeer <kzeery@mcmaster.ca>
2019-03-11 22:25:44 -0400

    Deploying Website

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", function(req, res) {
+    res.render("webpagecv");
+});
+
+
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("server has started!");
+});
